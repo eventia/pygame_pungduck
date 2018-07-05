14be1d31c8473f2174d8e7419971f2cfb49b7bff
eventia <eventia@gmail.com>
2018-07-06 00:05:20 +0900

    step03

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-# 02. 그리파일 부르기
+# 02. 배경 그림 부르기
 #
 # 1 - 파이게임 모듈을 가져온다
 # 모듈을 사용하기전 모듈을 설치해야 한다.
@@ -19,6 +19,8 @@ screen = pygame.display.set_mode((width, height))  # 게임창을 만든다
 
 # 3.이미지를 가져온다
 player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
 
 # 4 - 계속 화면이 보이도록 반복해서 실행한다
 while True:
@@ -27,7 +29,14 @@ while True:
     screen.fill((0,0,0))
 
     # 6. - 모든 요소들을 다시 그린다
-
+    for x in range(width//grass.get_width()+1):
+        for y in range(height//grass.get_height()+1):
+            screen.blit(grass,(x*100,y*100))
+    screen.blit(castle,(0,30))
+    screen.blit(castle,(0,135))
+    screen.blit(castle,(0,240))
+    screen.blit(castle,(0,345))
+    screen.blit(player,(100,100))
 
     # 7. - 화면을 다시 그린다
     pygame.display.flip()
